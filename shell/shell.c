b7dd3df264993a895a3b0fcf457e825e8e9edfa1
FelixProfi <felix@ssppeeaarrmmiint.com>
2026-02-15 12:58:59 +0000

    Auto-commit 2026-02-15 12:58:59

--- shell/shell.c
+++ shell/shell.c
@@ -57,7 +57,7 @@ void shell_main(void) {
             cls();
             cursor_x = 0;
             cursor_y = 0;          // курсор наверх
-            vga_update_cursor();
+            // vga_update_cursor();
         }
         else if (strcmp(input, "time") == 0) {
             print("Not implemented yet!\n", VGA_RED);
