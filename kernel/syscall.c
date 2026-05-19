7af930d34d7342c0bade89871f15c65aa55a11d6
FelixProfi <felix@ssppeeaarrmmiint.com>
2026-05-19 22:00:19 +0000

    Auto-commit 2026-05-19 22:00:19

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -7,16 +7,20 @@ extern void sys_print(const char* str, uint8_t color);
 extern void sys_cls(void);
 extern void sys_read_line(char* buffer, uint8_t color);
 
-void syscall_handler_c(uint32_t syscall_num, uint32_t arg1, uint32_t arg2) {
-    switch (syscall_num) {
+struct syscall_regs {
+    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+};
+
+void syscall_handler_c(struct syscall_regs* regs) {
+    switch (regs->eax) {
         case 1:
-            sys_print((const char*)arg1, (uint8_t)arg2);
+            sys_print((const char*)regs->ebx, (uint8_t)regs->ecx);
             break;
         case 2:
             sys_cls();
             break;
         case 3:
-            sys_read_line((char*)arg1, (uint8_t)arg2);
+            sys_read_line((char*)regs->ebx, (uint8_t)regs->ecx);
             break;
         default:
             break;
@@ -26,15 +30,14 @@ void syscall_handler_c(uint32_t syscall_num, uint32_t arg1, uint32_t arg2) {
 __attribute__((naked)) void syscall_handler_asm(void) {
     __asm__ __volatile__ (
         "pusha \n\t"
+
         "mov $0x10, %ax \n\t"
         "mov %ax, %ds \n\t"
         "mov %ax, %es \n\t"
 
-        "push %ecx \n\t"
-        "push %ebx \n\t"
-        "push %eax \n\t"
+        "push %esp \n\t"
         "call syscall_handler_c \n\t"
-        "add $12, %esp \n\t"
+        "add $4, %esp \n\t"
 
         "mov $0x23, %ax \n\t"
         "mov %ax, %ds \n\t"
